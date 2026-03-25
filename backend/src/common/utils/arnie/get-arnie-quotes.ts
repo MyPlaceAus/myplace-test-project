2fafc4cab518b9b568cb9bbbb13892adc4ea42f6
Zou Heng <hengxheng@hotmail.com>
2026-03-25 22:33:37 +1100

    feat:implement getArnieQuotes function to fetch quotes from provided URLs

--- backend/src/common/utils/arnie/get-arnie-quotes.ts
+++ backend/src/common/utils/arnie/get-arnie-quotes.ts
@@ -1,3 +1,26 @@
+import { httpGet } from './mock-http-interface';
 export type ArnieQuoteResult = { 'Arnie Quote': string } | { FAILURE: string };
 
-export const getArnieQuotes = async (urls: string[]) => {};
+export const getArnieQuotes = async (
+  urls: string[],
+): Promise<ArnieQuoteResult[]> => {
+  if (urls.length > 0) {
+    return await Promise.all(
+      urls.map(async (url) => {
+        try {
+          const response = await httpGet(url);
+          const { message } = JSON.parse(response.body) as { message: string };
+          if (response.status === 500) {
+            return { FAILURE: message };
+          }
+          return { 'Arnie Quote': message };
+        } catch (err) {
+          const errorMessage =
+            err instanceof Error ? err.message : 'Unknown error';
+          return { FAILURE: errorMessage };
+        }
+      }),
+    );
+  }
+  return [];
+};
