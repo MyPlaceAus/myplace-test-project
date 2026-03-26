9dfdefe887271c8aa8b973ed8ae5e7d25e228331
Zou Heng <hengxheng@hotmail.com>
2026-03-26 12:20:58 +1100

    fix:remove unnecessary peer dependencies from package-lock.json refactor:getArnieQuotes function to handle responses more clearly

--- backend/src/common/utils/arnie/get-arnie-quotes.ts
+++ backend/src/common/utils/arnie/get-arnie-quotes.ts
@@ -1,19 +1,20 @@
 import { httpGet } from './mock-http-interface';
 export type ArnieQuoteResult = { 'Arnie Quote': string } | { FAILURE: string };
 
-export const getArnieQuotes = async (
-  urls: string[],
-): Promise<ArnieQuoteResult[]> => {
+export const getArnieQuotes = async (urls: string[]) => {
   if (urls.length > 0) {
     return await Promise.all(
       urls.map(async (url) => {
         try {
           const response = await httpGet(url);
           const { message } = JSON.parse(response.body) as { message: string };
+          if (response.status === 200) {
+            return { 'Arnie Quote': message };
+          }
           if (response.status === 500) {
             return { FAILURE: message };
           }
-          return { 'Arnie Quote': message };
+          return { FAILURE: 'Quote not found' };
         } catch (err) {
           const errorMessage =
             err instanceof Error ? err.message : 'Unknown error';
